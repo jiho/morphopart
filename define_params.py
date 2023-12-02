5931795048442db2646d8e40e746254dfbdba263
Jean-Olivier Irisson <irisson@normalesup.org>
2023-12-02 11:30:38 +0100

    Prepare all parameters to explore for the pipeline

---
+++ define_params.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# Define all combinations of parameters for the pipeline
+#
+
+def expand_grid(data):
+    import pandas as pd
+    import itertools
+    rows = itertools.product(*data.values())
+    return(pd.DataFrame.from_records(rows, columns=data.keys()))
+
+params = expand_grid({
+    'instrument': ['uvp5sd','uvp6'],
+    'features': ['uvplib', 'mobilenet', 'dino'],
+    'n_obj_max': [2000000],
+    'replicate': [1, 2, 3],
+    'n_obj_sub': [10000, 50000, 100000, 250000, 500000, 1000000, 2000000],
+    # 'n_obj_sub': [250000, 500000, 1000000, 2000000],
+    # 'dim_reducer': ['UMAP', 'PCA'],
+    'dim_reducer': ['PCA'],
+    'n_clusters_tot': [200],
+    'linkage': ['ward'],
+    'n_clusters_eval': [5, 20, 100, 200],
+    'n_obj_dbcv': [25000]
+})
+
+# # test
+# params = expand_grid({
+#     'instrument': ['uvp5sd'],
+#     'features': ['mobilenet'],
+#     'n_obj_max': [50000],
+#     'replicate': [1],
+#     'n_obj_sub': [10000],
+#     'dim_reducer': ['UMAP'],
+#     'n_clusters_tot': [200],
+#     'linkage': ['ward'],
+#     'n_clusters_eval': [5],
+#     'n_obj_dbcv': [25000]
+# })
+
+# TODO possibly perform only one replicate in case n_obj_sub = n_obj_max? = remove the rows from the params_grid?
+
+print(f'Defined {params.shape[0]} combinations of parameters')
+
+params.to_csv('params_grid.csv', index=False)
